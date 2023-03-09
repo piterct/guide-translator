fbd1fafe5ffcec6f619940fd6b958076bf13c276
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-03-09 00:03:49 -0300

    Translate service

---
+++ Guide-Translator/Guide.Translate.Business/Services/TranslateService.cs
@@ -0,0 +1,22 @@
+﻿using Guide.Translate.Business.DTO;
+using Guide.Translate.Business.Interfaces.External;
+using Guide.Translate.Business.Interfaces.Services;
+using Guide.Translate.Business.Models;
+
+namespace Guide.Translate.Business.Services
+{
+    public class TranslateService : ITranslateService
+    {
+        private readonly IGPTFacade _gPTFacade;
+
+        public TranslateService(IGPTFacade gPTFacade)
+        {
+            _gPTFacade = gPTFacade;
+        }
+
+        public async Task<TranslatedDTO> Translate(TranslateModel translate)
+        {
+            return new TranslatedDTO { Translated = "Bla bla bla " };
+        }
+    }
+}
