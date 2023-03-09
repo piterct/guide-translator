b896c08c9ea51ed9afb774b99ac6372943ceee44
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-03-09 00:04:10 -0300

    AutoMapper Config

---
+++ Guide-Translator/Guide.Translate.Api/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Guide.Translate.Api.ViewModels;
+using Guide.Translate.Business.Models;
+
+namespace Guide.Translate.Api.AutoMapper
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<TranslateModel, TranslateViewModel>().ReverseMap();
+        }
+    }
+}
