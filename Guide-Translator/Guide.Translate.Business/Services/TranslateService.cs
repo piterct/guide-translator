5a27e56f545364818a1c1cb2493b1c41eba0fb2a
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-03-12 16:20:38 -0300

    LanguageChoice

--- Guide-Translator/Guide.Translate.Business/Services/TranslateService.cs
+++ Guide-Translator/Guide.Translate.Business/Services/TranslateService.cs
@@ -1,6 +1,7 @@
 ﻿using Guide.Translate.AntiCorruption.DTO;
 using Guide.Translate.AntiCorruption.Interfaces;
 using Guide.Translate.Business.DTO;
+using Guide.Translate.Business.Enum;
 using Guide.Translate.Business.Interfaces.Services;
 using Guide.Translate.Business.Models;
 using Microsoft.Extensions.Configuration;
@@ -18,11 +19,28 @@ namespace Guide.Translate.Business.Services
 
         public async Task<TranslatedDTO> Translate(TranslateModel translate)
         {
-            var chatGPTInput = new ChatGPTinputDTO($"Translate '{translate.Phrase}' to Brazilian Portuguese");
+            string phaseToTranslate = await LanguageChoice(translate);
+
+            var chatGPTInput = new ChatGPTinputDTO(phaseToTranslate);
 
             var translated = await _gPTFacade.Translate(chatGPTInput);
 
-            return new TranslatedDTO { Translated = translated.choices.Select(x=> x.text).FirstOrDefault() };
+            return new TranslatedDTO { Translated = translated.choices.Select(x => x.text).FirstOrDefault() };
+        }
+
+        private async Task<string> LanguageChoice(TranslateModel translate)
+        {
+            switch (translate.PhaseTo)
+            {
+                case ELanguages.BrazilianPortuguese:
+                    return $"Translate '{translate.Phrase}' to Brazilian Portuguese";
+                case ELanguages.Portuguese:
+                    return $"Translate '{translate.Phrase}' to Portugal Portuguese";
+                case ELanguages.Spanish:
+                    return $"Translate '{translate.Phrase}' to Spanish";
+                default:
+                    return $"Translate '{translate.Phrase}' to English";
+            }
         }
     }
 }
