10354f82f6f4ff92b4fd39a0ae7865933673ed8b
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-03-12 16:07:53 -0300

    GPT Facade

--- Guide-Translator/Guide.Translate.AntiCorruption/Facade/GPTFacade.cs
+++ Guide-Translator/Guide.Translate.AntiCorruption/Facade/GPTFacade.cs
@@ -1,8 +1,43 @@
-﻿using Guide.Translate.Business.Interfaces.External;
+﻿using Guide.Translate.AntiCorruption.DTO;
+using Guide.Translate.AntiCorruption.DTOs;
+using Guide.Translate.AntiCorruption.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Guide.Translate.AntiCorruption.Facade
 {
     public class GPTFacade : IGPTFacade
     {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public GPTFacade(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+
+        public async Task<ChatGPTOutputDTO> Translate(ChatGPTinputDTO translateModel)
+        {
+            var token = _configuration.GetSection("ChatGptKey");
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value);
+
+            var requestBody = JsonConvert.SerializeObject(translateModel);
+
+            var content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("https://api.openai.com/v1/completions", content);
+
+            return await response.Content.ReadFromJsonAsync<ChatGPTOutputDTO>();
+
+        }
+
+
     }
 }
