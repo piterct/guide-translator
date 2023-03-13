4e1fe14b807850c67db7a9541e5c43a9127623df
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-03-12 21:18:00 -0300

    TranslateModel

---
+++ Guide-Translator/Guide.Translate.Business/Models/TranslateModel.cs
@@ -0,0 +1,10 @@
+﻿using Guide.Translate.Business.Enum;
+
+namespace Guide.Translate.Business.Models
+{
+    public class TranslateModel
+    {
+        public string Phrase { get; set; }
+        public ELanguages PhaseTo { get; set; }
+    }
+}
