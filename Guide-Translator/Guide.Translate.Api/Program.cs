4b0ed077fed138242caee1c9651a65ca5e18bb0a
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-03-12 21:20:44 -0300

    AddHttpClient

--- Guide-Translator/Guide.Translate.Api/Program.cs
+++ Guide-Translator/Guide.Translate.Api/Program.cs
@@ -1,8 +1,7 @@
 using Guide.Translate.AntiCorruption.Facade;
-using Guide.Translate.Business.Interfaces.External;
+using Guide.Translate.AntiCorruption.Interfaces;
 using Guide.Translate.Business.Interfaces.Services;
 using Guide.Translate.Business.Services;
-using Microsoft.AspNetCore.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +12,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddHttpClient();
 
 // Services
 builder.Services.AddScoped<ITranslateService, TranslateService>();
