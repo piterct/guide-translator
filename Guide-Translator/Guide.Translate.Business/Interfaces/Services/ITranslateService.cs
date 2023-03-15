078b142682110e7bfc6014d467c17487a6f8c0e6
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-03-15 13:01:53 -0300

    ITranslate Service

---
+++ Guide-Translator/Guide.Translate.Business/Interfaces/Services/ITranslateService.cs
@@ -0,0 +1,10 @@
+﻿using Guide.Translate.Business.DTO;
+using Guide.Translate.Business.Models;
+
+namespace Guide.Translate.Business.Interfaces.Services
+{
+    public interface ITranslateService
+    {
+        Task<TranslatedDTO> Translate(TranslateModel translate);
+    }
+}
