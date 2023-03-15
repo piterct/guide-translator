3a70566ae1fda8fcc989ead78b7e3c1d46205865
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-03-15 13:01:27 -0300

    ChatBotInputDTO

--- Guide-Translator/Guide.Translate.AntiCorruption/DTOs/ChatGPTinputDTO.cs
+++ Guide-Translator/Guide.Translate.AntiCorruption/DTOs/ChatGPTinputDTO.cs
@@ -2,6 +2,17 @@
 {
     public class ChatGPTinputDTO
     {
-        public int MyProperty { get; set; }
+        public ChatGPTinputDTO(string promptCommand)
+        {
+            model = "text-davinci-003";
+            prompt = promptCommand;
+            max_tokens = 100;
+            temperature = 0.2M;
+        }
+
+        public string model { get; set; }
+        public string prompt { get; set; }
+        public int max_tokens { get; set; }
+        public decimal temperature { get; set; }
     }
 }
