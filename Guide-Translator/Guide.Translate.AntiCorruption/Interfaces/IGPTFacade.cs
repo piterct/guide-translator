723a6865ec9180410cec52e5961b85995e7a1c6c
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-03-19 17:32:15 -0300

    IGPTFacade

---
+++ Guide-Translator/Guide.Translate.AntiCorruption/Interfaces/IGPTFacade.cs
@@ -0,0 +1,10 @@
+﻿using Guide.Translate.AntiCorruption.DTO;
+using Guide.Translate.AntiCorruption.DTOs;
+
+namespace Guide.Translate.AntiCorruption.Interfaces
+{
+    public interface IGPTFacade
+    {
+        Task<ChatGPTOutputDTO> Translate(ChatGPTinputDTO TranslateModel);
+    }
+}
