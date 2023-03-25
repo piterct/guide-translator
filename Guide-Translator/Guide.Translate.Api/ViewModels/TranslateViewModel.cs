8449f3d5a93a81ff54ebc74e9d3ea309cb34a33f
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-03-25 01:57:59 -0300

    translate view model

---
+++ Guide-Translator/Guide.Translate.Api/ViewModels/TranslateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Guide.Translate.Business.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace Guide.Translate.Api.ViewModels
+{
+    public class TranslateViewModel
+    {
+        [Required(ErrorMessage = "The field {0} is Required")]
+        [StringLength(500, ErrorMessage = "The field {0} must at least between {2} and {1} caracteres", MinimumLength = 2)]
+        public string Phrase { get; set; }
+        [Required(ErrorMessage = "The field {0} is Required")]
+        public ELanguages PhaseTo { get; set; }
+    }
+}
