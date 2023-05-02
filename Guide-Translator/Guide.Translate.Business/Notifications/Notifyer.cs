f01b8b0cdf2880a676ab05b86a7b4fad3a28f139
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-05-01 22:48:56 -0300

    Notifyer Class

---
+++ Guide-Translator/Guide.Translate.Business/Notifications/Notifyer.cs
@@ -0,0 +1,27 @@
+﻿namespace Guide.Translate.Business.Notifications
+{
+    public class Notifyer : INotifyer
+    {
+        private List<Notify> _notifications;
+
+        public Notifyer()
+        {
+            _notifications = new List<Notify>();
+        }
+
+        public void Handle(Notify notificacao)
+        {
+            _notifications.Add(notificacao);
+        }
+
+        public List<Notify> GetNotifications()
+        {
+            return _notifications;
+        }
+
+        public bool HasNotification()
+        {
+            return _notifications.Any();
+        }
+    }
+}
