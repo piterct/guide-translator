27cff0ae159c7e448cd551b5e23693333c07d9d5
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-05-05 00:20:17 -0300

    InvalidOperation class

--- Guide-Translator/Guide.Translate.Api/Controllers/MainController.cs
+++ Guide-Translator/Guide.Translate.Api/Controllers/MainController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Guide.Translate.Business.Notifications;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Guide.Translate.Api.Controllers
 {
     public abstract class MainController : Controller
     {
+        private readonly INotifyer _notifyer;
 
+        public MainController(INotifyer notifyer)
+        {
+            _notifyer = notifyer;
+        }
+
+        protected bool InvalidOperation()
+        {
+            return !_notifyer.HasNotification();
+        }
     }
 }
