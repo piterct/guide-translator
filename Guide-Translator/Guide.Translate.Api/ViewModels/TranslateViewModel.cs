8a7de1187bad35537a0ea1519afd4e45181d7c8b
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-05-07 15:13:40 -0300

    modified MinimumLength

--- Guide-Translator/Guide.Translate.Api/ViewModels/TranslateViewModel.cs
+++ Guide-Translator/Guide.Translate.Api/ViewModels/TranslateViewModel.cs
@@ -6,7 +6,7 @@ namespace Guide.Translate.Api.ViewModels
     public class TranslateViewModel
     {
         [Required(ErrorMessage = "The field {0} is Required")]
-        [StringLength(500, ErrorMessage = "The field {0} must at least between {2} and {1} caracteres", MinimumLength = 2)]
+        [StringLength(500, ErrorMessage = "The field {0} must at least between {2} and {1} caracteres", MinimumLength = 5)]
         public string Phrase { get; set; }
         [Required(ErrorMessage = "The field {0} is Required")]
         public ELanguages PhaseTo { get; set; }
