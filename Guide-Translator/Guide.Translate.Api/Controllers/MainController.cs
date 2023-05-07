7980b5e2b735759749658e15e8b64e03d0610fd4
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-05-07 15:12:44 -0300

    Customer response using by maincontroller

--- Guide-Translator/Guide.Translate.Api/Controllers/MainController.cs
+++ Guide-Translator/Guide.Translate.Api/Controllers/MainController.cs
@@ -1,5 +1,6 @@
 ﻿using Guide.Translate.Business.Notifications;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Guide.Translate.Api.Controllers
 {
@@ -15,6 +16,45 @@ namespace Guide.Translate.Api.Controllers
         protected bool InvalidOperation()
         {
             return !_notifyer.HasNotification();
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            if (!modelState.IsValid) NotifyInvalidModelState(modelState);
+            return CustomResponse();
+        }
+
+        protected void NotifyInvalidModelState(ModelStateDictionary modelState)
+        {
+            var erros = modelState.Values.SelectMany(e => e.Errors);
+            foreach (var erro in erros)
+            {
+                var erroMsg = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+                ErrorNotify(erroMsg);
+            }
+        }
+
+        protected void ErrorNotify(string message)
+        {
+            _notifyer.Handle(new Notify(message));
+        }
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (InvalidOperation())
+            {
+                return Ok(new
+                {
+                    success = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                erros = _notifyer.GetNotifications().Select(n => n.Message)
+            });
         }
     }
 }
