8c424cc949ea8cd0ffb107976a58cf2bdc5efd85
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-05-15 22:17:08 -0300

    Translate Controller

--- Guide-Translator/Guide.Translate.Api/Controllers/TranslateController.cs
+++ Guide-Translator/Guide.Translate.Api/Controllers/TranslateController.cs
@@ -2,6 +2,7 @@
 using Guide.Translate.Api.ViewModels;
 using Guide.Translate.Business.Interfaces.Services;
 using Guide.Translate.Business.Models;
+using Guide.Translate.Business.Notifications;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,13 +10,13 @@ namespace Guide.Translate.Api.Controllers
 {
     [ApiController]
     [Route("/api/v1/[controller]")]
-    public class TranslateController : BaseController
+    public class TranslateController : MainController
     {
         private readonly ILogger<TranslateController> _logger;
         private readonly ITranslateService _translateService;
         private readonly IMapper _mapper;
 
-        public TranslateController(ILogger<TranslateController> logger, ITranslateService translateService, IMapper mapper)
+        public TranslateController(ILogger<TranslateController> logger, ITranslateService translateService, IMapper mapper, INotifyer notifyer) : base(notifyer)
         {
             _logger = logger;
             _translateService = translateService;
@@ -30,7 +31,7 @@ namespace Guide.Translate.Api.Controllers
             try
             {
                 if (!ModelState.IsValid)
-                    return BadRequest(model);
+                    return CustomResponse(model);
 
                 var translate = await _translateService.Translate(_mapper.Map<TranslateModel>(model));
 
